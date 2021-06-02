bba49afde70b5f70111d57806f209875703d0e9e
MikaelOmarTarhuni <84583156+MikaelOmarTarhuni@users.noreply.github.com>
2021-06-02 19:57:12 +0200

    task 1.2
    change and add pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello world');
-let favoriteFood = "pizza";
-document.write(favoriteFood)
+let pokemonList = [{name: "Pikachu", type: ["electric"], hight: 0.4},
+                   {name: "Charmander", type: ["fire"], hight: 0.6},
+                   {name: "Squirtle", type: ["water"], hight: 0.5},
+                   {name: "Bulbasaur", type: ["water", "grass"], hight: 0.7}];
+console.log(pokemonList);
+    
