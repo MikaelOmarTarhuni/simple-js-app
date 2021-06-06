c40e895432705a40980ecb6146151549f1a4ef8a
MikaelOmarTarhuni <84583156+MikaelOmarTarhuni@users.noreply.github.com>
2021-06-06 20:01:16 +0200

    task 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,39 @@
-let pokemonList = [{name: "Pikachu", type: ["electric"], hight: 0.4},
-                   {name: "Charmander", type: ["fire"], hight: 0.6},
-                   {name: "Squirtle", type: ["water"], hight: 0.5},
-                   {name: "Bulbasaur", type: ["water", "grass"], hight: 0.7}];
-console.log(pokemonList);
+let pokemonList = [
+    {
+        name: "Pikachu",
+        type: ["electric"],
+        hight: 0.4
+    },           
+    {
+        name: "Charmander",
+        type: ["fire"],
+        hight: 0.6
+    },
+    {
+        name: "Squirtle",
+        type: ["water"],
+        hight: 0.5
+    },
+    {
+        name: "Charizard",
+        type: ["fire"],
+        hight: 1.7
+    },
+    {
+        name: "Bulbasaur",
+        type: ["water", "grass"],
+        hight: 0.7
+    }
     
+];
+
+for (let i=0; i < pokemonList.length; i++){
+    if (pokemonList[i].hight < 1.7 && pokemonList[i].hight > 0.3)
+    {
+        document.write(pokemonList[i].name + " (" + pokemonList[i].hight + ")" + " Wow, that is a small pokemon!");
+    }
+    else if (pokemonList[i].hight > 1.7 )
+    {
+        document.write(pokemonList[i].name + " (" + pokemonList[i].hight + ")" +" Wow, that is a big pokemon!");
+    }
+}
