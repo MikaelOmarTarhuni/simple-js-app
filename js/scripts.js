8f8788efc3926eeed184f953ca442537faf9bbf3
MikaelOmarTarhuni <84583156+MikaelOmarTarhuni@users.noreply.github.com>
2021-06-13 14:08:39 +0200

    task 1.6

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let pokemonRepository = (function () {
+var pokemonRepository = (function () {
     let pokemonList = [
         {
             name: "Pikachu",
@@ -24,22 +24,88 @@ let pokemonRepository = (function () {
             name: "Bulbasaur",
             type: ["water", "grass"],
             hight: 0.7
-        }
+        },
     ];
-  
-    return {
-      add: function(pokemon) {
-        pokemonList.push(pokemon);
-      },
-      getAll: function() {
+
+    function add (pokemon) {
+        if (
+            typeof pokemon === "object" &&
+            "name" in pokemon &&
+            "height" in pokemon &&
+            "type" in pokemon
+        ) {
+            pokemonList.push(pokemon);
+        } else {
+            console.log("Pokemon is not correct");        
+        }
+    }
+
+    function getAll() {
         return pokemonList;
-      }
+    }
+
+    function addListItem(pokemon) {
+
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listpokemon = document.createElement("li");
+        let button = document.createElement("button");
+        button.innerText = pokemon.name
+        button.classList.add("button-class");
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
+    }
+
+    return {
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem,
     };
   })();
 
 
+  pokemonRepository.add({name: "Arbok", hight: 3.5, type:["poison"]});
+
 pokemonRepository.getAll().forEach(function(pokemon) {
-    document.write( pokemon.name + " (" + pokemon.hight +") " )});
+    pokemonRepository.addListItem(pokemon)
+
+
+    /*var size;
+    if (pokemon.hight > 1) {
+        size = "wow, that is big";
+    } else {
+        size = "it is small pokemon";
+    }
+
+    var result;
+    pokemon.type.forEach(function (typeitem){
+        if (typeitem == "grass") {
+            result ='<span style="color:green;"> ';
+        } else if (typeitem == "fire") {
+            result = '<span style="color:red;"> ';
+        } else if (typeitem == "electric") {
+            result ='<span style="color:yellow;"> ';
+        } else if (typeitem == "animal") {
+            result ='<span style="color:black;"> ';
+        } else if (typeitem == "water") {
+            result ='<span style="color:blue;"> ';
+        } 
+
+    });
+    document.write( 
+        '<div class="box">' +
+        pokemon.name +
+        "(height: " +
+        pokemon.hight +
+        "m" +
+        ")" +
+        "<br>" +
+        size +
+        result +
+        "<br>" +
+        pokemon.type +
+        "<br>" +
+        "</div>");*/
+    });
 
 /**for (let i=0; i < pokemonList.length; i++){
     if (pokemonList[i].hight < 1.7 && pokemonList[i].hight > 0.3)
